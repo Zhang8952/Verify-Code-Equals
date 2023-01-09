554a84bb15f12a6001601281cef0fbecd7983aee
Zhang8952 <107750044+Zhang8952@users.noreply.github.com>
2023-01-09 14:29:19 +0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,10 @@
+public void Verify(View view)
+	{
+		EditText UnEdit = (EditText)findViewById(R.id.EditTextUn);
+		String Un = new String();
+                if(Un.equals("abc"))
+                {
+                }
+                else
+                {
+                }
